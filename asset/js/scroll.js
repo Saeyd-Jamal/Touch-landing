1d0949bcf76bc323cfc9253847c57ba4e5b91a27
Al-Saeyd J Al-Akhrs <79516145+Saeyd-Jamal@users.noreply.github.com>
2024-11-21 07:47:17 +0200

    add animation in  the all page

---
+++ asset/js/scroll.js
@@ -0,0 +1,23 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+    reset: true
+});
+
+// origin : top
+sr.reveal(`nav, .top__files , .text__work ,.text__client ,.top__footer`);
+
+
+// origin : left 
+sr.reveal(`.logo__hero, .text__hero, .img__about, .partial__about, .text__service, .partial__files, .left__files , .left_partners , .left__work ,.text__footer `, { origin: 'left' });
+sr.reveal(`.service__box`, { interval: 200,origin: 'left' }); // عدة عناصر والانتظار بينهم
+
+// origin : right
+sr.reveal(`.right__hero, .img__hero, .text__about, .img__service , .right_partners , .right__work`, { origin: 'right' });
+
+// origin : bottom
+sr.reveal(`.center__partners , .review__arrow , .bottom__footer`, { origin: 'bottom' });
+sr.reveal(`.links__footer`, { interval: 100,origin: 'bottom' }); // عدة عناصر والانتظار بينهم
+sr.reveal(`.card__about, .card__file , .img__partners , .top__work , .img__client `, { interval: 200,origin: 'bottom' }); // عدة عناصر والانتظار بينهم
